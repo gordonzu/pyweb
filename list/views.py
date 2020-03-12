da4caa059584c501f0e099b79e103c7f51395629
gordonzu <gordonzu@outlook.com>
2020-03-11 20:24:24 -0400

    basic view now return minimal html

--- list/views.py
+++ list/views.py
@@ -1,8 +1,11 @@
+#######################################
+# views.py
+# gordon Zuehlke 3-11-20
+#######################################
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
-def home_page():
-    pass
-
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
 
