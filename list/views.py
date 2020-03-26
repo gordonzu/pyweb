0baa2832a65d642d2a0bdabeea291f91bc9b38f6
gordonzu <gordonzu@outlook.com>
2020-03-25 21:39:40 -0400

    Before refactoring new input entries do not persist

--- list/views.py
+++ list/views.py
@@ -1,13 +1,11 @@
-#######################################
-# views.py
-# gordon Zuehlke 3-11-20
-#######################################
 
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
 
 
 
